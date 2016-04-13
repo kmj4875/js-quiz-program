ac9e75c721778aa7021f0e2748ef369bbf656fb9
kmj4875 <minjae.kim@wustl.edu>
2016-04-13 18:26:18 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,13 +1,59 @@
-// variables
+/ variables
 var correct = 0;
 
-// prompt
-var answer1 = prompt("ask a question...");
+// Questions
+var answer1 = prompt("How many states are there in the United States?");
 
-// if statement
-if ( answer1.toUpperCase() === 'MAGIC' ) {
+if ( answer1.toUpperCase() === '50' ) {
  correct += 1;
 }
 
-// output results
-document.write("<p>write something to page...</p>");
+var answer2 = prompt("How many planets are there in the solar system?");
+
+if ( answer2.toUpperCase() === '9' ) {
+ correct += 1;
+}
+
+var answer3 = prompt("How many continents are there?");
+
+if ( answer3.toUpperCase() === '7' ) {
+ correct += 1;
+}
+
+var answer4 = prompt("How old is planet Earth?");
+
+if ( answer4.toUpperCase() === '4.54 billion years' ) {
+ correct += 1;
+}
+
+var answer5 = prompt("How old is the St. Louis Arch?");
+
+if ( answer5.toUpperCase() === '53 years old' ) {
+ correct += 1;
+}
+
+var answer6 = prompt("An integer from 100 through 999, inclusive, is to be chosen at random. What is the probability that the number chosen will have 0 as at least 1 digit?");
+
+if ( answer5.toUpperCase() === '171/900' ) {
+ correct += 1;
+}
+
+if (correct < 2) {
+    document.write("That went horrible");
+}
+
+if (correct == 3) {
+    document.write("You got 50% right!");
+}
+
+if (correct == 4) {
+    document.write("4 out of 6 right! ");
+}
+
+if (correct == 5) {
+    document.write("You got 5 out of 6 right! Smarter than the average bear. ");
+}
+
+if (correct == 6) {
+    document.write("WOW! 100% congratulations you get nothing :) ");
+}
