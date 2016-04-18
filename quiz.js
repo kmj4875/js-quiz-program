811791ee49a186bbdb191c76b2c6f4df52142518
kmj4875 <minjae.kim@wustl.edu>
2016-04-18 17:59:28 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,59 +1,43 @@
-// variables
+// quiz begins, no answers correct
 var correct = 0;
 
-// Questions
-var answer1 = prompt("How many states are there in the United States?");
-
-if ( answer1 == '50' ) {
- correct += 1;
+// ask questions
+function question(questionText, realAnswer){
+ var answer1 = prompt(questionText);
+if ( answerInput.toUpperCase() === realAnswer ) {
+ correct += 1; 
+}
 }
 
-var answer2 = prompt("How many planets are there in the solar system?");
 
-if ( answer2 == '9' ) {
- correct += 1;
+// var answer2 = prompt("Name a programming language that's also a snake");
+// if ( answer2.toUpperCase() === 'PYTHON' ) {
+//  correct += 1; 
+// }
+// var answer3 = prompt("What language do you use to style web pages?");
+// if ( answer3.toUpperCase() === 'CSS' ) {
+//  correct += 1; 
+// }
+// var answer4 = prompt("What language do you use to build the structure of web pages?");
+// if ( answer4.toUpperCase() === 'HTML' ) {
+//  correct += 1; 
+// }
+// var answer5 = prompt("What language do you use to add interactivity to a web page?");
+// if ( answer5.toUpperCase() === 'JAVASCRIPT' ) {
+//  correct += 1; 
+// }
+
+// output results
+document.write("<p>You got " + correct + " out of 5 questions correct.<p>");
+
+// output rank
+if ( correct === 5 ) {
+  document.write("<p><strong>You earned a gold crown!</strong></p>");  
+} else if (correct > 3) {
+  document.write("<p><strong>You earned a silver crown.</strong></p>");  
+} else if (correct > 2) {
+  document.write("<p><strong>You earned a bronze crown.</strong></p>");  
+} else {
+  document.write("<p><strong>No crown for you. You need to study.</strong></p>");
 }
-
-var answer3 = prompt("How many continents are there?");
-
-if ( answer3 == '7' ) {
- correct += 1;
-}
-
-var answer4 = prompt("How old is planet Earth?");
-
-if ( answer4.toUpperCase() === '4.54 billion years' ) {
- correct += 1;
-}
-
-var answer5 = prompt("How old is the St. Louis Arch?");
-
-if ( answer5.toUpperCase() === '53 years old' ) {
- correct += 1;
-}
-
-var answer6 = prompt("An integer from 100 through 999, inclusive, is to be chosen at random. What is the probability that the number chosen will have 0 as at least 1 digit?");
-
-if ( answer5.toUpperCase() === '171/900' ) {
- correct += 1;
-}
-
-if (correct < 2) {
-    document.write("That went horrible");
-}
-
-if (correct == 3) {
-    document.write("You got 50% right!");
-}
-
-if (correct == 4) {
-    document.write("4 out of 6 right! ");
-}
-
-if (correct == 5) {
-    document.write("You got 5 out of 6 right! Smarter than the average bear. ");
-}
-
-if (correct == 6) {
-    document.write("WOW! 100% congratulations you get nothing ");
-}
+               
